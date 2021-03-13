a0e28289692486d97f03db6a03fc08f331581564
wallysonlima <wallyson.n.a.lima@gmail.com>
2021-03-13 15:19:20 -0300

    Created login screen

--- app/src/main/java/wallyson/lima/sourceball/MainActivity.kt
+++ app/src/main/java/wallyson/lima/sourceball/MainActivity.kt
@@ -1,12 +1,35 @@
-Unesp2021&
 package wallyson.lima.sourceball
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnClean: Button
+    private lateinit var btnLogin: Button
+    private lateinit var textViewRegister: TextView
+    private lateinit var editTextFacebook: EditText
+    private lateinit var editTextPassword: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initialize()
+        
+        btnClean.setOnClickListener {
+            editTextFacebook.setText("")
+            editTextPassword.setText("")
+        }
+    }
+
+    fun initialize() {
+        btnClean = findViewById(R.id.buttonClean)
+        btnLogin = findViewById(R.id.buttonLogin)
+        textViewRegister = findViewById(R.id.textViewRegister)
+        editTextFacebook = findViewById(R.id.editTextFacebook)
+        editTextPassword = findViewById(R.id.editTextPassword)
     }
 }
