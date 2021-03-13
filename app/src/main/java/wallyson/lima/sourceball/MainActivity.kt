320d55f63da68f8313561e277f8c727d9a8e8221
wallysonlima <wallyson.n.a.lima@gmail.com>
2021-03-13 14:06:28 -0300

    first commit

---
+++ app/src/main/java/wallyson/lima/sourceball/MainActivity.kt
@@ -0,0 +1,12 @@
+Unesp2021&
+package wallyson.lima.sourceball
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
